70e2190dd2ed3fc09b0cc30aa24703a993ea288a
Marta Skowronska <marta_skowronska@epam.com>
2020-08-20 15:08:35 +0200

    Improved the look but the movies are presented outside the application

--- movie-app/src/components/MovieCard.js
+++ movie-app/src/components/MovieCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/movieComponentsStyle.css';
 
 export default function MovieCard (props){
   return  (
-    <div className="MovieCard">
-      <img className="movieposter" alt="movie poster" src={props.image}/>
-      <div>
-        <div>
-          <p>{props.title}</p>
-          <p>{props.genre}</p>
-        </div>
-          <p>{props.year}</p>
+    <div className="movieCard">
+      <img className="moviePoster" alt="movie poster" src={props.image}/>
+      <div className="movieAllInfo">
+          <div className="movieInfo">
+              <p>{props.title}</p>
+              <p>{props.genre}</p>
+          </div>
+          <div className="movieYear">{props.year}</div>
       </div>
     </div>
   )}
--- movie-app/src/components/MoviesList.js
+++ movie-app/src/components/MoviesList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieCard from './MovieCard'
+import '../styles/movieComponentsStyle.css';
 
 let movies=[
 {
@@ -48,7 +49,7 @@ let movies=[
 
 export default function MoviesList(){
     return  (
-    <div className="movieList">
+    <>
     {
       movies.map((movie)=>(
         <MovieCard title={movie.title}
@@ -58,5 +59,5 @@ export default function MoviesList(){
         key={movie.id}/>
       ))
     }
-    </div>
+    </>
   )}
