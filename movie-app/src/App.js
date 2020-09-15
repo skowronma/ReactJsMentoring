f322fc42319fe5e1cb8863e18c1f5dcb0f3221b5
msk <marta_skowronska@epam.com>
2020-09-15 16:58:24 +0200

    Fixed the movie count

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -12,75 +12,7 @@ import * as actions from './actions/actionCreator';
 import { connect } from 'react-redux';
 
 var pageName = 'netflixroulette';
-let allMovies=[
-  {
-    title: 'Pulp Fiction',
-    genre: 'Crime',
-    year: '1994',
-    movieUrl : 'Some url',
-    overview: 'Pulp Fiction is a 1994 American neo-noir black comedy crime film written and directed by Quentin Tarantino, who conceived it with Roger Avary. Starring John Travolta, Samuel L. Jackson, Bruce Willis, Tim Roth, Ving Rhames, and Uma Thurman, it tells several stories of criminal Los Angeles. The title refers to the pulp magazines and hardboiled crime novels popular during the mid-20th century, known for their graphic violence and punchy dialogue',
-    duration : '154 min',
-    image: './images/1.jpg',
-    rating: '4.3',
-    id: '1'
-  },
-  {
-    title: 'Bohemian Rapsody',
-    genre: 'Drama, Biography, Music',
-    year: '2003',
-    movieUrl : 'Some url',
-    overview: 'The story of the legendary British rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Live Aid (1985)',
-    image: './images/2.jpg',
-    duration: '134 min',
-    rating: '4.1',
-    id: '2'
-  },
-  {
-    title: 'Kill Bill: Vol 2',
-    genre: 'Oscar Winning Movie',
-    year: '1994',
-    movieUrl : 'Some url',
-    overview: 'The Bride continues her quest of vengeance against her former boss and lover Bill, the reclusive bouncer Budd, and the treacherous, one-eyed Elle',
-    image: './images/3.jpg',
-    duration: '120 min',
-    rating: '4.5',
-    id: '3'
-  },
-  {
-    title: 'Avengers: Infinity War',
-    genre: 'Action & Adventure',
-    year: '2004',
-    movieUrl : 'Some url',
-    overview: 'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.',
-    image: './images/4.jpg',
-    duration: '154 min',
-    rating: '4.0',
-    id: '4'
-  },
-  {
-    title: 'Inception',
-    genre: 'Action & Adventure',
-    year: '2003',
-    movieUrl : 'Some url',
-    overview: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
-    image: './images/5.jpg',
-    duration: '144 min',
-    rating: '3.9',
-    id: '5'
-  },
-  {
-    title: 'Reservoir dogs',
-    genre: 'Oscar Winning Movie',
-    year: '1994',
-    movieUrl : 'Some url',
-    overview: 'When a simple jewelry heist goes horribly wrong, the surviving criminals begin to suspect that one of them is a police informant',
-    image: './images/6.jpg',
-    duration: '104 min',
-    rating: '4.0',
-    id: '6'
-  }
-]
-
+var allMovies=[];
 function mapStateToProps(state) {
   return {
     movies: state.data,
--- movie-app/src/components/MoviesContainer.js
+++ movie-app/src/components/MoviesContainer.js
@@ -3,15 +3,15 @@ import MoviesCount from './MoviesCount';
 import MoviesList from './MoviesList';
 import SortComponent from './SortComponent';
 
-const movieCount = 39;
-
 export default function MoviesContainer(props){
   return (
     <div>
       <SortComponent/>
-      <MoviesCount movieCount={movieCount} />
       {props.movies != null ?
-      <MoviesList movies={props.movies} onClick={(movie) => props.onClick(movie)}/>
+      <>
+        <MoviesCount movieCount={props.movies.length} />
+        <MoviesList movies={props.movies} onClick={(movie) => props.onClick(movie)}/>
+      </>
         : <div>Loading</div>
       }
     </div>
